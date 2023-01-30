a683d026d9588c794f3915811025104af7bdb242
jmedina <ndgns2@gmail.com>
2023-01-30 14:38:02 -0700

    Add future notes to self

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { data } from "./data";
 import MoviesContainer from "./MoviesContainer/MoviesContainer";
 import "./App.css";
