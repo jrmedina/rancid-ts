ce11fffe2a0f63ae709150308cb8a5b185cb00d3
jmedina <ndgns2@gmail.com>
2023-01-31 16:46:27 -0700

    Remove all traces of sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { data } from "./resources/data";
 import MoviesContainer from "./MoviesContainer/MoviesContainer";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
