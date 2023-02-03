6eea6c5b2a691d98fdfe7084a726b973105624cb
jmedina <ndgns2@gmail.com>
2023-02-02 17:51:11 -0700

    Format detailed date

--- src/MovieCard/MovieCard.tsx
+++ src/MovieCard/MovieCard.tsx
@@ -19,15 +19,16 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  console.log(new Date(movie.release_date).toLocaleDateString());
+
   return (
     <>
       {movie && (
         <div className="MovieCard">
           <h3 className="title">{movie.title}</h3>
           <p>{movie.average_rating}</p>
-          <p>{movie.release_date}</p>
           <p>{movie.runtime}</p>
-          <p>{movie.release_date}</p>
+          <p>{new Date(movie.release_date).toLocaleDateString()}</p>
           <p>{movie.budget}</p>
           <p>{movie.overview}</p>
           <p>{movie.genres}</p>
