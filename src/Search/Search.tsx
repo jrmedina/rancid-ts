b6a447b7b186ff92765e188e72700d57052a69fa
jmedina <ndgns2@gmail.com>
2023-02-14 14:56:10 -0700

    Refactor search function

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import "./Search.css"
+import "./Search.css";
 
 interface SearchProps {
   searchMovies: React.Dispatch<React.SetStateAction<any>>;
 }
 const Search: React.FC<SearchProps> = ({ searchMovies }) => {
-const [input, setInput] = useState()
+  const [input, setInput] = useState();
   return (
     <form>
-        <p>Search</p>
-      <input type="text" value={input} onChange={(e) => searchMovies(e.target.value)}/>
+      <p>Search</p>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => searchMovies(e.target.value)}
+      />
     </form>
   );
-}
+};
 
 export default Search;
